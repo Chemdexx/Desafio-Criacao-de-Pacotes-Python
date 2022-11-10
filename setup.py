991c3016ca05022ce5684ab0337e005251b3d7a2
Chemdexx <carlos.sekine@gmail.com>
2022-11-10 00:27:00 -0300

    Inclusão dos arquivos binários

--- setup.py
+++ setup.py
@@ -9,11 +9,12 @@ with open("requirements.txt") as f:
 setup(
     name="Image_processing",
     version="0.0.1",
-    author="Sekine",    
-    description="Image Processing Package using Skimage", 
+    author="Sekine",
+    
+    description="Image Processing Package using Skimage",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/Chemdexx/Desafio-Criacao-de-Pacotes-Python"
+    url="https://github.com/Chemdexx/Desafio-Criacao-de-Pacotes-Python",
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8',
